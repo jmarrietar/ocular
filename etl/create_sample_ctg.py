e60d9a7024ef024699a7e570900122d0ceb7974d
Jose Miguel Arrieta Ramos <jmarrietar@gmail.com>
2021-11-20 14:30:01 -0500

    Small changes sample

--- etl/create_sample_ctg.py
+++ etl/create_sample_ctg.py
@@ -2,45 +2,40 @@
 # -*- coding: utf-8 -*-
 
 
-"""
-os.makedirs('sample@{}'.format(SAMPLE), exist_ok=True)
-os.makedirs('sample@{}/train/0'.format(SAMPLE), exist_ok=True)
-os.makedirs('sample@{}/train/1'.format(SAMPLE), exist_ok=True)
-"""
-
 import os
 import random
 import shutil 
-
-from os import listdir
+from os.path import exists
+from os import makedirs
 from PIL import Image
 
 
-CLASS = 0
-SAMPLE = 20000
-SOURCE_DIR = "train_voets"
+CLASSES = [0, 1]
+SAMPLE = 10000
+SOURCE_DIR = "data/voets/train"
+random.seed(42)
 
-list_images = os.listdir(os.path.join(SOURCE_DIR, str(CLASS)))
-sampled_list = random.sample(list_images, int(SAMPLE/2)) 
+for CLASS in CLASSES:
 
-
-for image_name in sampled_list: 
+    list_images = os.listdir(os.path.join(SOURCE_DIR, str(CLASS)))
+    sampled_list = random.sample(list_images, int(SAMPLE/2)) 
     
-    image_source_path = os.path.join(SOURCE_DIR, str(CLASS), image_name)
+    # Create directories
+    if not exists("sample@{}/train/{}".format(SAMPLE, CLASS)):
+        makedirs("sample@{}/train/{}".format(SAMPLE, CLASS))
     
-    if image_source_path.endswith('.jpg'):
-        try:
-            img = Image.open(image_source_path) 
-            img.verify()
-        except (IOError, SyntaxError) as e:
-            print(e)
-            print('Bad file:', image_name)
-            
-    destination = "sample@{}/train/{}/{}".format(SAMPLE, CLASS, image_name)
-    dest = shutil.copyfile(image_source_path, destination) 
-
-
-
-
-
-
+    for image_name in sampled_list: 
+        print(image_name)
+        
+        image_source_path = os.path.join(SOURCE_DIR, str(CLASS), image_name)
+        
+        if image_source_path.endswith('.jpg'): # Already preprocessed image
+            try:
+                img = Image.open(image_source_path) 
+                img.verify()
+            except (IOError, SyntaxError) as e:
+                print(e)
+                print('Bad file:', image_name)
+                
+        destination = "sample@{}/train/{}/{}".format(SAMPLE, CLASS, image_name)
+        dest = shutil.copyfile(image_source_path, destination)
